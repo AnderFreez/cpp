280c806aa5bd8e8f64e6ffc2cdca1a292dbe21fb
AnderFreez <sia.spb.m167@gmail.com>
2019-09-30 16:13:37 +0300

    Update taskWithArray.cpp

--- afterKumir/afterKumir/taskWithArray.cpp
+++ afterKumir/afterKumir/taskWithArray.cpp
@@ -253,15 +253,19 @@ namespace taskWithArray
 
 		for (int i = 0; i < size; i++)
 		{
-			if (a[i] > max1)
+			if (a[i] <= max2)
 			{
-				max2 = max1;
-				max1 = a[i];
 				continue;
 			}
 			if (a[i] > max2 && a[i] <= max1)
 			{
 				max2 = a[i];
+				continue;
+			}
+			if (a[i] > max1)
+			{
+				max2 = max1;
+				max1 = a[i];
 				continue;
 			}
 		}
