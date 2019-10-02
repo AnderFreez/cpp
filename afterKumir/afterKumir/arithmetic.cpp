ea5370e6299da852225cf0963f961e51eb8ed12d
AnderFreez <sia.spb.m167@gmail.com>
2019-10-02 21:50:56 +0300

    i

---
+++ afterKumir/afterKumir/arithmetic.cpp
@@ -0,0 +1,23 @@
+#include "Limits.h"
+#include <iostream>
+using namespace std;
+namespace arithmetic
+{
+	// +
+	int plus(int* a, int size, int a)
+	{
+		int ed = INT_MIN;
+		int des = INT_MIN;
+		int sot = INT_MIN;
+		int d = INT_MIN;
+		ed = a % 10;
+		a = a / 10;
+		des = a % 10;
+		a = a / 10;
+		sot = a % 10;
+
+	}
+
+
+
+}
