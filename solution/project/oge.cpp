9e48610f1b8a9917eb744188f56383539bdb317b
AnderFreez <sia.spb.m167@gmail.com>
2019-11-05 21:03:40 +0300

    лоаув

--- solution/project/oge.cpp
+++ solution/project/oge.cpp
@@ -148,6 +148,170 @@ namespace oge
 		{
 			cout << "NO";
 		}
-		
+	}
+
+	void task461()
+	{
+		int amount = 0;
+		int amountDigits = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 2 == 1 && number % 3 == 0)
+			{
+				++amount;
+			}
+			++amountDigits;
+			cin >> number;
+		}
+		cout << amount << endl << amountDigits;
+	}
+
+	void task481()
+	{
+		int amount = 0;
+		int sum = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 2 == 0 && number % 5 == 0)
+			{
+				++amount;
+			}
+			sum += number;
+			cin >> number;
+		}
+		cout << sum << endl << amount;
+	}
+
+	void task501()
+	{
+		int amount = 0;
+		int sum = 0;
+		int number = 0;
+		while (number != 0)
+		{
+			if (number % 2 == 0)
+			{
+				sum += number;
+			}
+			++amount;
+			cin >> number;
+		}
+		cout << amount << endl << sum;
+	}
+
+	void task561()
+	{
+		int amount = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 5 == 0 && number % 2 == 0)
+			{
+				++amount;
+			}
+			cin >> number;
+		}
+		cout << amount;
+	}
+
+	void task581()
+	{
+		int amount = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 9 == 0 && number % 2 == 0)
+			{
+				++amount;
+			}
+			cin >> number;
+		}
+		cout << amount;
+	}
+
+	void task601()
+	{
+		int sum = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 7 == 0 && number % 10 == 2)
+			{
+				sum += number;
+			}
+			cin >> number;
+		}
+		cout << sum;
+	}
+
+	void task621()
+	{
+		int sum = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 6 == 0 && number % 10 == 4)
+			{
+				sum += number;
+			}
+			cin >> number;
+		}
+		cout << sum;
+	}
+
+	void task641()
+	{
+		int sum = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 8 == 0 && number % 10 == 6)
+			{
+				sum += number;
+			}
+			cin >> number;
+		}
+		cout << sum;
+	}
+
+	void task661()
+	{
+		int sum = 0;
+		int number = 0;
+		cin >> number;
+		while (number != 0)
+		{
+			if (number % 3 == 0 && number % 10 == 4)
+			{
+				sum += number;
+			}
+			cin >> number;
+		}
+		cout << sum;
+	}
+
+	void task721()
+	{
+		int n = 5;
+		int number = 0;
+		int min = INT_MAX;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> number;
+			if (number % 10 == 4 && number < min)
+			{
+				min = number;
+			}
+		}
+		cout << min;
 	}
 }
--- solution/project/oge.h
+++ solution/project/oge.h
@@ -11,4 +11,24 @@ namespace oge
 	void task341();
 
 	void task361();
+
+	void task461();
+
+	void task481();
+
+	void task501();
+
+	void task561();
+
+	void task581();
+
+	void task601();
+
+	void task621();
+
+	void task641();
+
+	void task661();
+
+	void task721();
 }
